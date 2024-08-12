f56a2660a82faf75a929cd4ccd549794afe79e21
chirag singh <94050461+chiragdhouni@users.noreply.github.com>
2024-08-12 23:36:19 +0530

    added lost item detail widget

--- lib/features/lost_item_screen/ui/widgets/lost_item_card.dart
+++ lib/features/lost_item_screen/ui/widgets/lost_item_card.dart
@@ -13,8 +13,27 @@ class _LostItemCardState extends State<LostItemCard> {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => LostItemDetail(
+                      item: widget.item,
+                    )));
+      },
       child: Container(
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.grey[800],
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: Offset(0, 1), // changes position of shadow
+              ),
+            ]),
         child: ListTile(
             title: Text(widget.item.name),
             subtitle: Text(
@@ -26,6 +45,40 @@ class _LostItemCardState extends State<LostItemCard> {
                 color: Colors.white, // Customize the text style as needed
               ),
             )),
+      ),
+    );
+  }
+}
+
+class LostItemDetail extends StatefulWidget {
+  final LostItem item;
+  const LostItemDetail({super.key, required this.item});
+
+  @override
+  State<LostItemDetail> createState() => _LostItemDetailState();
+}
+
+class _LostItemDetailState extends State<LostItemDetail> {
+  @override
+  Widget build(BuildContext context) {
+    LostItem item = widget.item;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.item.name),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Text(item.name),
+            Text(item.description),
+            Text(item.location.coordinates.first.toString()),
+            Text(item.location.coordinates.last.toString()),
+            Text(item.dateLost.toString()),
+            Text(item.owner.address),
+            Text(item.owner.email),
+            Text(item.contactInfo),
+          ],
+        ),
       ),
     );
   }
